07c57dc03ae97ec1442c7b76c5df86436589b484
luisgtm12 <120407727+luisgtm12@users.noreply.github.com>
2024-06-09 13:54:10 -0500

    Implementando InfoTooltip

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./Header.js";
 import Footer from "./Footer.js";
 import Main from "./Main.js";
@@ -8,8 +10,10 @@ import Register from "./Register.js";
 import EditProfilePopup from "./EditProfilePopup.js";
 import AddPlacePopup from "./AddPlacePopup.js";
 import EditAvatarPopup from "./EditAvatarPopup.js";
-import ImagePopup from './ImagePopup.js'
+import ImagePopup from "./ImagePopup.js";
 import Login from "./Login.js";
+import ProtectedRoute from "./ProtectedRoute";
+import InfoTooltip from "./InfoTooltip.js";
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
@@ -18,12 +22,14 @@ function App() {
   const [selectCard, setSelectCard] = useState(null);
   const [currentUser, setCurrentUser] = useState({});
   const [cards, setCards] = useState([]);
+  const [isLogged, setIsLogged] = useState(true);
+
 
   useEffect(() => {
-    api.getCards().then((cards)=>{
+    api.getCards().then((cards) => {
       setCards(cards);
-    })
-  },[]);
+    });
+  }, []);
 
   useEffect(() => {
     api.defaultProfile().then((res) => {
@@ -56,91 +62,116 @@ function App() {
 
   function handleCardLike(card) {
     // Verifica una vez más si a esta tarjeta ya le han dado like
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
-    
-    if(isLiked) {
-      api.deleteLike(card._id).then((newCard => {
-        setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
-      }))
+    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+
+    if (isLiked) {
+      api.deleteLike(card._id).then((newCard) => {
+        setCards((state) =>
+          state.map((c) => (c._id === card._id ? newCard : c))
+        );
+      });
     } else {
-      api.addLike(card._id).then((newCard => {
-        setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
-      }))
+      api.addLike(card._id).then((newCard) => {
+        setCards((state) =>
+          state.map((c) => (c._id === card._id ? newCard : c))
+        );
+      });
     }
   }
-  
-  function handleCardDelete(card){
+
+  function handleCardDelete(card) {
     api.deleteCard(card._id).then((res) => {
       setCards((state) => state.filter((c) => c._id !== card._id));
     });
   }
 
   function handleUpdateUser(data) {
-    api.updateProfile(data).then(()=>{setCurrentUser({...currentUser,...data});
-    setIsEditProfilePopupOpen(false);
-  })
+    api.updateProfile(data).then(() => {
+      setCurrentUser({ ...currentUser, ...data });
+      setIsEditProfilePopupOpen(false);
+    });
   }
 
   function handleUpdateAvatar(data) {
-    api.updateAvatar(data).then((userData)=>{
+    api.updateAvatar(data).then((userData) => {
       setCurrentUser(userData);
       setIsEditAvatarPopupOpen(false);
-    })
+    });
   }
-  
-  function handleAddPlaceSubmit(card){
-    api.addCards(card).then((newCard)=>{
-      setCards([newCard,...cards]);
+
+  function handleAddPlaceSubmit(card) {
+    api.addCards(card).then((newCard) => {
+      setCards([newCard, ...cards]);
       setIsAddPlacePopupOpen(false);
-    })
+    });
   }
 
   return (
     <>
       <div className="page">
         <CurrentUserContext.Provider value={currentUser}>
-          
-          {/*<Header />
-          <Main
-            onEditAvatarClick={handleEditAvatarClick}
-            onEditProfileClick={handleEditProfileClick}
-            onAddPlaceClick={handleAddPlaceClick}
-            isOpen={[
-              isEditProfilePopupOpen,
-              isAddPlacePopupOpen,
-              isEditAvatarPopupOpen,
-            ]}
-            onClose={closePopups}
-            onCardClick={handleCardClick}
-            onCardLike={handleCardLike}
-            onCardDelete={handleCardDelete}
-            cards={cards}
-          /> */}
-          <Register/>
-          <Login/>
-          <EditAvatarPopup
-          isOpen={isEditAvatarPopupOpen}
-          onClose={closePopups}
-          onUpdateAvatar={handleUpdateAvatar}
-          />
-          <EditProfilePopup
-          isOpen={isEditProfilePopupOpen}
-          onClose={closePopups}
-          onUpdateUser={handleUpdateUser}
-          />
-          <AddPlacePopup
-          isOpen={isAddPlacePopupOpen}
-          onClose={closePopups}
-          onAddPlace={handleAddPlaceSubmit}
-          />
-          <ImagePopup
-        title={selectCard?.name || ""}
-        image={selectCard?.link || ""}
-        isOpen={!!selectCard}
-        onclose={closePopups}        
-        />
+          <Routes>
+            <Route element={<ProtectedRoute loggedIn={isLogged} />}>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Header 
+                      signText={"Cerrar sesión"}
+                      email={"luisgtm23@hotmail.com"}
+                    />
+                    <Main
+                      onEditAvatarClick={handleEditAvatarClick}
+                      onEditProfileClick={handleEditProfileClick}
+                      onAddPlaceClick={handleAddPlaceClick}
+                      isOpen={[
+                        isEditProfilePopupOpen,
+                        isAddPlacePopupOpen,
+                        isEditAvatarPopupOpen,
+                      ]}
+                      onClose={closePopups}
+                      onCardClick={handleCardClick}
+                      onCardLike={handleCardLike}
+                      onCardDelete={handleCardDelete}
+                      cards={cards}
+                    />
+                    <EditAvatarPopup
+                      isOpen={isEditAvatarPopupOpen}
+                      onClose={closePopups}
+                      onUpdateAvatar={handleUpdateAvatar}
+                    />
+                    <EditProfilePopup
+                      isOpen={isEditProfilePopupOpen}
+                      onClose={closePopups}
+                      onUpdateUser={handleUpdateUser}
+                    />
+                    <AddPlacePopup
+                      isOpen={isAddPlacePopupOpen}
+                      onClose={closePopups}
+                      onAddPlace={handleAddPlaceSubmit}
+                    />
+                    <ImagePopup
+                      title={selectCard?.name || ""}
+                      image={selectCard?.link || ""}
+                      isOpen={!!selectCard}
+                      onclose={closePopups}
+                    />
+                    
+                  </>
+                }
+              />
+            </Route>
+          </Routes>
+          {/* */}
+      
         </CurrentUserContext.Provider>
       </div>
+      <InfoTooltip
+                    isOpen={true}
+                    name={"tooltip"}
+                    title={"Simon"}
+                    
+                    />
       <Footer />
     </>
   );
---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,25 @@
+import closeIcon from "../images/close-icon.png"
+import correct from "../images/Corretc.png"
+function InfoTooltip ({name,title,isOpen,onClose}){
+  return(
+    <div id={`modal-${name}`} className={`modal modal_type_${name} ${isOpen ? "modal__opened" : ""}`}>
+          <div className="modal__contain">
+            <img
+              src={closeIcon}
+              id={`close-icon-${name}`}
+              alt="icono de cierre"
+              className="modal__close-icon"
+              onClick={onClose}
+            />
+            <div className="modal__content">
+              <img src={correct}
+                className="modal__content_img"
+                />
+              <h4 className="modal__contain-title">{title}</h4>
+            </div>
+            
+          </div>
+        </div>
+  )
+}
+export default InfoTooltip;
